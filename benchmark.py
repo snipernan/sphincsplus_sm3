f245c81b3aad92a3e7fe663b7cfdf0afb0d6c4c8
Wang Sunan <wangsunan1234567@163.com>
2023-10-27 10:26:50 +0800

    Deletion of unnecessary documents and addition of LICENSE

--- benchmark.py
+++ benchmark.py
@@ -7,10 +7,8 @@ from subprocess import DEVNULL, run
 
 implementations = [
                     ('sm3-avx2', ['sm3']),      
-                    #('sha2-avx2', ['sha2']),
-                    #('shake-avx2', [ 'shake' ]),
-                    #('shake-a64', [ 'shake' ]),
-                    ('ref', [ 'sm3' ]),
+                    ('sha2-avx2', ['sha2']),
+                    ('ref', [ 'sm3', 'sha2' ]),
                    ]
 
 options = ["f", "s"]
